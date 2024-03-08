0eefa7107f3a6caa6f44dec0f168e50dce3b1735
Julian F <131391694+JulianFrProgrammiert@users.noreply.github.com>
2024-03-08 19:18:39 +0100

    Initial project version

---
+++ target/debug/deps/weather_in_command-941cae3a38d1c615.d
@@ -0,0 +1,5 @@
+/media/joel/Volume/Dev/Projects/weather_in_command/target/debug/deps/libweather_in_command-941cae3a38d1c615.rmeta: src/main.rs
+
+/media/joel/Volume/Dev/Projects/weather_in_command/target/debug/deps/weather_in_command-941cae3a38d1c615.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/weather_in_command-dce74b3b885b4ebf.d
@@ -0,0 +1,5 @@
+/media/joel/Volume/Dev/Projects/weather_in_command/target/debug/deps/libweather_in_command-dce74b3b885b4ebf.rmeta: src/main.rs
+
+/media/joel/Volume/Dev/Projects/weather_in_command/target/debug/deps/weather_in_command-dce74b3b885b4ebf.d: src/main.rs
+
+src/main.rs:
